a3c48390e8ef2889fc67bf477f71a60875e6c0b7
Jason Mayer <jason.mayer@clarivate.com>
2020-04-08 22:54:47 -0400

    endpoint ot return a jwt token. next step make endpoint to get invoice data from janus

--- app/controllers/authorization_controller.rb
+++ app/controllers/authorization_controller.rb
@@ -3,18 +3,29 @@ class AuthorizationController < ApplicationController
 
   # POST /authorizations
   def create
-    render json: [], status: :ok
+    if params[:invoice_id] == '0211' && params[:secret_key] == '4891'
+      render json: payload, status: :ok
+    else
+      render json: {error: 'Invalid invoice or secret key'}, status: 401
+    end
   end
 
   # DELETE /authorizations/1
   def delete
-    @authorization.destroy
-    render json: {deleted: true}
+    render json: [], status: 306
   end
 
   private
-    # Only allow a trusted parameter "white list" through.
-    def authorization_params
-      params.require(:authorization).permit(:invoice_id, :secret_key)
-    end
+
+  def payload(invoice=nil)
+    payload = {
+      exp: 3.hours.from_now.to_i,
+      data: {
+        first_name: 'Carlos',
+        last_name: 'Danger',
+        invoice: '1234'
+      }
+    }
+    {auth_token: JWT.encode(payload, ENV['SUPER_SECRET_KEY_BASE']) }
+  end
 end
